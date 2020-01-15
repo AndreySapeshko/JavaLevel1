4a5b4cc0cf7006885a19f85ec2159fd1c4bc8d89
Andrey Sapeshko <andrdd17@gmail.com>
2020-01-15 21:10:33 +0300

    Задание 1 и 2

--- hw1/src/main/java/HomeWork1.java
+++ hw1/src/main/java/HomeWork1.java
@@ -1,5 +1,18 @@
+import org.graalvm.compiler.replacements.StandardGraphBuilderPlugins;
+
 public class HomeWork1 {
     public static void main(String[] args) {
-        
+        byte a = 127;
+        short b = 32000;
+        int c = 1015;
+        long d = 92233L;
+        float e = 1.5f;
+        double g = 7.48;
+        char h = 'd';
+        boolean j = true;
+        System.out.println(a + " | " + b + " | " + c + " | " + d + " | " + e +
+                " | " + g + " | " + h + " | " + j);
+
+
     }
 }
